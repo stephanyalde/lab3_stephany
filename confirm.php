b4e121165e74a4ea67393b50a9f8bbc93903e7d5
Alcaide <salcaid@ilstu.edu>
2026-01-29 17:23:39 -0600

    adding files

---
+++ confirm.php
@@ -0,0 +1,10 @@
+<html>
+
+<body>
+<?php echo $_POST["fname"]; ?><br>
+<?php echo $_POST["ema"]; ?><br>
+<?php echo $_POST["age"]; ?><br>
+</body>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form action="confirm.php" method="post">
+  Name: <input type="text" name="fname"><br>
+  Email: <input type="text" name="ema"><br>
+  Age: <input type="text" name="age"><br>
+
+<input type="submit">
+</form>
+
+</body>
+
+</html>
